c442b7739a363aa453a260700cd26a81ec5b06cf
cristobal hernandez chavez <cristobalhdzch@gmail.com>
2022-12-12 18:34:54 -0700

    Add default sorting. Refactor table layout to components

--- src/views/Mainview.jsx
+++ src/views/Mainview.jsx
@@ -1,32 +1,36 @@
 import React, { useContext } from "react";
 
-import ItemData from "../components/ItemData/ItemData";
-
 import Grid from "@mui/material/Grid";
+
+import TableLayout from "../components/UiHelpers/TableLayout";
+import ItemData from "../components/ItemData/ItemData";
 
 import itemInfo from "../DataArray/ItemInfo.js";
 import itemContext from "../store/itemXp-context";
+// import DropdownSort from "../components/UiHelpers/DropdownSort";
 
 const Mainview = () => {
   const itemCtx = useContext(itemContext);
   const { totalItemXp } = itemCtx;
 
-  const itemList = itemInfo.map((data) => {
+  const filterSortHandler = () => {
+
+  }
+
+  const sortedItemInfo = itemInfo.sort((a, b) => a.level - b.level);
+
+  const itemList = sortedItemInfo.map((data) => {
     return (
-      <React.Fragment key={data.id}>
-        <Grid item xs="auto" xl={3}></Grid>
-        <Grid item xs={12} md={10} xl={6} marginBottom={1}>
-          <ItemData
-            id={data.id}
-            name={data.name}
-            member={data.member}
-            imageUrl={data.imageUrl}
-            level={data.level}
-            experience={data.experience}
-          />
-        </Grid>
-        <Grid item xs="auto" xl={3}></Grid>
-      </React.Fragment>
+      <TableLayout id={data.id}>
+        <ItemData
+          id={data.id}
+          name={data.name}
+          member={data.member}
+          imageUrl={data.imageUrl}
+          level={data.level}
+          experience={data.experience}
+        />
+      </TableLayout>
     );
   });
 
@@ -36,6 +40,9 @@ const Mainview = () => {
       <Grid item xs={12}>
         <h1>{Math.floor(totalItemXp).toLocaleString()}</h1>
       </Grid>
+      {/* <Grid item xs={12}>
+        <DropdownSort />
+      </Grid> */}
       <Grid container justifyContent="center">
         {itemList}
       </Grid>

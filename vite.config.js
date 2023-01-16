ec99ad178cbb913e8995295474739a4df3ce0b87
cristobal hernandez chavez <cristobalhdzch@gmail.com>
2023-01-15 19:54:35 -0700

    Fix base url in viteconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/banked-xp-app/",
+  base: "/banked-experience-app/",
   plugins: [react()],
 });
